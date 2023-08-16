b5cff1f708ee1cae3da8965fdd318ef7d7306351
Vishal Patange <vishalpatange69@gmail.com>
2023-08-16 17:23:12 +0530

    New User

--- Component/Roboshop/Frontend.sh
+++ Component/Roboshop/Frontend.sh
@@ -2,25 +2,6 @@
 
 echo " I am Frontend User"
 
-1. 
-
-# yum install nginx -y
-# systemctl enable nginx
-# systemctl start nginx
-
-2. 
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-3. 
-
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
 USER_ID =$(id -u)
 
 if [ $USER_ID -ne 0 ] ; then 
@@ -34,6 +15,17 @@ echo " COnfiguring Frontend"
 
 yum install nginx -y
 
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 
 
