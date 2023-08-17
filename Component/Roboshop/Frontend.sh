25f47f2f31b0d9d38e4a71c1111dfc5d9a56e334
Vishal Patange <vishalpatange69@gmail.com>
2023-08-17 10:52:37 +0530

    New User

--- Component/Roboshop/Frontend.sh
+++ Component/Roboshop/Frontend.sh
@@ -27,7 +27,15 @@ else
    echo -e "\e[31m Failure \e[0m"    
 fi
 
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+echo -n "Downloading the component :"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ]; then
+   echo -e "\e[32m Success \e[0m"
+else 
+   echo -e "\e[31m Failure \e[0m"    
+fi
+
+
 # cd /usr/share/nginx/html
 # rm -rf *
 # unzip /tmp/frontend.zip
