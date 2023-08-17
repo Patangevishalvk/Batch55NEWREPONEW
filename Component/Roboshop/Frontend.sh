857746b222c0fb7c09919e8c5925b55f0d549836
Vishal Patange <vishalpatange69@gmail.com>
2023-08-17 14:36:01 +0530

    New User

--- Component/Roboshop/Frontend.sh
+++ Component/Roboshop/Frontend.sh
@@ -1,9 +1,10 @@
 #!bin/bash
 
 set -e 
+
+USER_ID=$(id -u)
 Component=Frontend
 LOGFILE="/tmp/${COMPONENT}.log"
-USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ] ; then 
    echo -e "\e[33m Script is expected to executed by the root user \e[0m \n \t Example: \n\t\t sudo bash Wrapper1.sh Frontend"
@@ -47,7 +48,7 @@ rm -rf *
 stat $?
 
 echo -n "Extracting ${COMPONENT} :"
-unzip /tmp/${COMPONENT}.zip     &>>  ${LOGFILE}
+unzip /tmp/Frontend.zip     &>>  ${LOGFILE}
 mv ${COMPONENT}-main/* . 
 mv static/* . 
 rm -rf  ${COMPONENT}-main README.md 
