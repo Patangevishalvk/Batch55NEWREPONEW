2f59f10d17a61eac6c2ce94208b789a2800ec960
Vishal Patange <vishalpatange69@gmail.com>
2023-08-17 10:46:41 +0530

    New User

--- Component/Roboshop/Frontend.sh
+++ Component/Roboshop/Frontend.sh
@@ -21,6 +21,11 @@ fi
 echo "enablling and starting the Frontend :"
 systemctl enable nginx
 systemctl start nginx
+if [ $? -eq 0 ]; then
+   echo -e "\e[32m Success \e[0m"
+else 
+   echo -e "\e[32m Failure \e[0m"    
+fi
 
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 # cd /usr/share/nginx/html
