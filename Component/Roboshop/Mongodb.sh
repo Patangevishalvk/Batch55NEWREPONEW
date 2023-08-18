67ea1a2b5a511d538212cdf42e4178cdb495bd36
Vishal Patange <vishalpatange69@gmail.com>
2023-08-18 11:48:43 +0530

    New User

--- Component/Roboshop/Mongodb.sh
+++ Component/Roboshop/Mongodb.sh
@@ -34,7 +34,7 @@ systemctl start mongod   &>> ${LOGFILE}
 stat $?
 
 echo -n "enablling the ${COMPONENT} visibility :"
-sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/mongodb.conf
+sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 stat $?
 
 echo -n "Starting the ${COMPONENT} :"
