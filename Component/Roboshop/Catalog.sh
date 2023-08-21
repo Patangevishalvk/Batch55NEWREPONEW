8f4a6eb3a63da7e6825c4ea3e1c0edee3bbd0bff
Vishal Patange <vishalpatange69@gmail.com>
2023-08-21 11:15:04 +0530

    New User

--- Component/Roboshop/Catalog.sh
+++ Component/Roboshop/Catalog.sh
@@ -52,7 +52,10 @@ mv ${COMPONENT} -main ${COMPONENT}
 chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
 stat $?
 
- 
+echo -n "Generating the ${COMPONENT} artifacts :"
+cd /home/${APPUSER}/${COMPONENT} 
+npm install &>> ${LOGFILE}
+stat $?
 
 
 # echo -n "Downloading the ${COMPONENT} schema: "
