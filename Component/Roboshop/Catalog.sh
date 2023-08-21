5143cffc44b1a8c96d67b273d8f6471d3db068a8
Vishal Patange <vishalpatange69@gmail.com>
2023-08-21 11:29:09 +0530

    New User

--- Component/Roboshop/Catalog.sh
+++ Component/Roboshop/Catalog.sh
@@ -29,12 +29,9 @@ echo -n "Installing Nodejs repos :"
 yum install nodejs -y &>> ${LOGFILE}
 stat $?
 
-
-
 echo -n "Downloading the ${COMPONENT} :"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
-
 
 echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory :"
 cd /home/${APPUSER}/
