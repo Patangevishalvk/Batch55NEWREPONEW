8ac7d30a867123f18a15b47c80ef028595c242ff
Vishal Patange <vishalpatange69@gmail.com>
2023-08-21 10:55:58 +0530

    New User

--- Component/Roboshop/Catalog.sh
+++ Component/Roboshop/Catalog.sh
@@ -47,6 +47,11 @@ rm -rf ${COMPONENT}           &>> ${LOGFILE}
 unzip -o /tmp/${COMPONENT}.zip &>> ${LOGFILE}
 stat $?
 
+echo -n "Changing the Ownership :"
+mv ${COMPONENT} -main ${COMPONENT}
+chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
+stat $?
+
 
 
 
