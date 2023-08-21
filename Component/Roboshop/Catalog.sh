5b3da0a656d279fca96dbc995605381fb3cc785d
Vishal Patange <vishalpatange69@gmail.com>
2023-08-21 11:40:26 +0530

    New User

--- Component/Roboshop/Catalog.sh
+++ Component/Roboshop/Catalog.sh
@@ -16,7 +16,9 @@ if [ $? -eq 0 ]; then
    echo -e "\e[32m Success \e[0m"
 else 
    echo -e "\e[31m Failure \e[0m"    
-   exit 2 fi
+   exit 2 
+fi
+
 }
 
 echo -n "Configuring ${COMPONENT} repos :"
