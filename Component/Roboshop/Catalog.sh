f8cda0a56a2ee0016e82f67f549720baa86c6ab2
Vishal Patange <vishalpatange69@gmail.com>
2023-08-21 09:48:01 +0530

    New User

--- Component/Roboshop/Catalog.sh
+++ Component/Roboshop/Catalog.sh
@@ -1,3 +1,52 @@
 #!bin/bash
+set -e 
 
-echo " I am Catalog User"
+USER_ID=$(id -u)
+COMPONENT=Catalog
+LOGFILE="/tmp/${COMPONENT}.log"
+APPUSER="roboshop"
+if [ $USER_ID -ne 0 ] ; then 
+   echo -e "\e[33m Script is expected to executed by the root user \e[0m \n \t Example: \n\t\t sudo bash Wrapper1.sh Catalog"
+   exit 1
+fi
+
+stat () {
+    
+    if [ $? -eq 0 ]; then
+   echo -e "\e[32m Success \e[0m"
+else 
+   echo -e "\e[31m Failure \e[0m"    
+   exit 2 
+    fi
+}
+
+echo -n "Configuring ${COMPONENT} repos :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+stat $?
+
+echo -n "Installing Nodejs repos :"
+yum install nodejs -y &>> ${LOGFILE}
+stat $?
+
+echo -n "Creating application User account :"
+useradd roboshop
+stat $?
+
+
+
+# echo -n "Downloading the ${COMPONENT} schema: "
+# curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
+# stat $? 
+
+# echo -n "Extracing the ${COMPONENT} Schema  :"
+# cd /tmp 
+# unzip -o ${COMPONENT}.zip        &>> ${LOGFILE} 
+# stat $? 
+
+# echo -n "Injecting ${COMPONENT} Schema : "
+# cd ${COMPONENT}-main
+# mongo < catalogue.js    &>>  ${LOGFILE}
+# mongo < users.js        &>>  ${LOGFILE}
+# stat $? 
+
+# echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
