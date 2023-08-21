12343e22b4d886c10a66300a707801c5587e941a
Vishal Patange <vishalpatange69@gmail.com>
2023-08-21 12:41:56 +0530

    New User

--- Component/Roboshop/Catalog.sh
+++ Component/Roboshop/Catalog.sh
@@ -37,12 +37,14 @@ fi
 echo -n "Downloading the ${COMPONENT} :"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" &>> ${LOGFILE}
 set -e 
+stat $?
 
 echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory"
 cd /home/${APPUSER}/
 rm -rf ${COMPONENT}             &>> ${LOGFILE}
 unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
 stat $?
+
 
 
 
