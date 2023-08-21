4be4c0149a1a55f841ebf54090d4474645fc8e7b
Vishal Patange <vishalpatange69@gmail.com>
2023-08-21 11:38:25 +0530

    New User

--- Component/Roboshop/Catalog.sh
+++ Component/Roboshop/Catalog.sh
@@ -30,7 +30,7 @@ yum install nodejs -y &>> ${LOGFILE}
 stat $?
 
 id ${APPUSER} &>> ${LOGFILE}
-if  [ $?  -eq 0 ] ; then 
+if  [ $? -ne 0 ] ; then 
 echo -n "Creating Application User Account :"
 useradd roboshop
 stat $?
